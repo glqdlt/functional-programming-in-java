3d141fd236d9aa7d7bf364adc8b2b8dbb9a7ee2c
glqdlt <dlfdnd0725@gmail.com>
2019-03-16 17:54:02 +0900

    max 구현

--- src/main/java/com/glqdlt/ex/SomeFunction.java
+++ src/main/java/com/glqdlt/ex/SomeFunction.java
@@ -1,5 +1,6 @@
 package com.glqdlt.ex;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -7,7 +8,44 @@ import java.util.List;
  * 2019-03-16
  */
 public class SomeFunction {
-    public static int max(List<Integer> list){
-        return list.get(0);
+    public static int sum(List<Integer> arr){
+        if(arr.size() == 0){
+            throw new RuntimeException("Empty List");
+        }
+        if(arr.size() == 1){
+            return arr.get(0);
+        }
+        LinkedList<Integer> temp = new LinkedList<>();
+        temp.addAll(arr);
+        Integer first = temp.get(0);
+        Integer second = temp.get(1);
+        temp.set(0,first+second);
+        temp.remove(1);
+        return sum(temp);
+    }
+
+
+    public static int max(List<Integer> arr){
+
+        if(arr.size() == 0){
+            throw new RuntimeException("Empty List");
+        }
+        if(arr.size() == 1){
+            return arr.get(0);
+        }
+
+        LinkedList<Integer> temp = new LinkedList<>();
+        temp.addAll(arr);
+
+        int first = temp.get(0);
+        int second = temp.get(1);
+
+        if (first > second) {
+            temp.remove(1);
+        } else {
+            temp.remove(0);
+        }
+
+        return max(temp);
     }
 }
--- src/test/java/com/glqdlt/ex/SomeFunctionTest.java
+++ src/test/java/com/glqdlt/ex/SomeFunctionTest.java
@@ -1,19 +1,40 @@
 package com.glqdlt.ex;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author glqdlt
  * 2019-03-16
  */
 public class SomeFunctionTest {
+    List<Integer> fixture;
+
+    @Before
+    public void setUp() throws Exception {
+        fixture = Arrays.asList(9,8,9,1, 6,6,7, 85,1,2,99);
+    }
 
     @Test
     public void 리스트에서가장큰수찾기() throws Exception {
-        int res = SomeFunction.max(Arrays.asList(1, 6, 8));
-        Assert.assertEquals(8,res);
+        Assert.assertEquals(99,SomeFunction.max(fixture));
+    }
+
+    @Test
+    public void 겁나긴리스트에서가장큰수찾기() throws Exception {
+        List<Integer> asd = IntStream.rangeClosed(0, 2999).boxed().collect(Collectors.toList());
+        Assert.assertEquals(2999,SomeFunction.max(asd));
+    }
+
+    @Test
+    public void 리스트에서() throws Exception {
+        Assert.assertEquals(233,SomeFunction.sum(fixture));
     }
 }
