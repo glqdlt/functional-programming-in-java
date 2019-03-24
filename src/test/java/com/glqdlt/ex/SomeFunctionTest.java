a7aa911a64de5cd3e54c2a8ff333e91523dae63d
glqdlt <dlfdnd0725@gmail.com>
2019-03-24 22:29:09 +0900

    주석 추가

--- src/test/java/com/glqdlt/ex/SomeFunctionTest.java
+++ src/test/java/com/glqdlt/ex/SomeFunctionTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,6 +26,12 @@ public class SomeFunctionTest {
         Assert.assertEquals(99,SomeFunction.max(fixture));
     }
 
+    /**
+     * stack size 가 기본 defaults 이상으로 늘려줘야 OOM 이 발생하지 않는다.
+     * Hotspot 기준으로 -Xss 옵션으로 스택사이즈를 늘려줄 수 있다.
+     * @see <a href ="https://stackoverflow.com/questions/3700459/how-to-increase-the-java-stack-size">https://stackoverflow.com/questions/3700459/how-to-increase-the-java-stack-size</a>*
+     * @throws Exception
+     */
     @Test
     public void 겁나긴리스트에서가장큰수찾기() throws Exception {
         List<Integer> asd = IntStream.rangeClosed(0, 2999).boxed().collect(Collectors.toList());
